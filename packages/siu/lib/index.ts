34c4a222dca953a56d5d1e7c9f58c9268c8e1c0f
buns <kuafujs@126.com>
2020-11-25 17:21:20 +0800

    refactor: `@siujs/cli` rename to `siu`

---
+++ packages/siu/lib/index.ts
@@ -0,0 +1,25 @@
+import { applyPlugins, PkgCommand } from "@siujs/core";
+import { initApp } from "@siujs/init-app";
+
+interface CommonOptions {
+	pkgs?: string;
+	[x: string]: any;
+}
+
+/**
+ *
+ * run client command
+ *
+ * @param cmd client command
+ * @param options client command payload options
+ */
+export async function runCmd<T extends CommonOptions>(cmd: PkgCommand | "init", options: T) {
+	if (cmd === "init") {
+		await initApp(options as any);
+		return;
+	}
+
+	const { pkgs, ...rest } = options || {};
+
+	await applyPlugins(cmd, pkgs, rest);
+}
