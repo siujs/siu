33a181ce64085fb0e3bc92d1bff4c79a8ad37106
buns <kuafujs@126.com>
2020-11-27 10:28:38 +0800

    feat(rollup): add dts generate logic

--- packages/rollup/lib/index.ts
+++ packages/rollup/lib/index.ts
@@ -1,8 +1,6 @@
-export * from "rollup";
-
-export * from "./config";
-
-export * from "./esbuildService";
-
 export * from "./builder/base";
 export * from "./builder/babel-plugin-import";
+export * from "./config";
+export * from "./dts";
+export * from "./esbuildService";
+export * from "rollup";
