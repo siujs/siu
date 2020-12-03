0f01b16840619e3f35aedbb4d33d37cdc6dfd236
buns <kuafujs@126.com>
2020-12-03 10:44:47 +0800

    refactor: mv `plugin-*` into git:`siujs/plugins`

--- packages/plugin-vui/lib/index.ts
+++
@@ -1,8 +0,0 @@
-// import { plugin } from "@siujs/core";
-
-// const plug = plugin({});
-
-// plug.creation.start(onCreationStart);
-// plug.creation.process(onCreationProcess);
-// plug.creation.complete(onCreationComplete);
-// plug.creation.error(onCreationError);
