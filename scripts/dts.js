3c0f7c57f75e82c0bb9817ff49617daf43db0e17
buns <kuafujs@126.com>
2020-12-09 11:09:47 +0800

    chore: rename 'siu'=>'cli'

--- scripts/dts.js
+++ scripts/dts.js
@@ -7,7 +7,7 @@ const shelljs = require("shelljs");
 
 const packagesRoot = path.resolve(__dirname, "../packages/");
 
-const pkgDirList = ["utils", "core", "cmd-build", "cmd-deps", "cmd-glint", "cmd-init", "cmd-publish", "siu"];
+const pkgDirList = ["utils", "core", "cmd-build", "cmd-deps", "cmd-glint", "cmd-init", "cmd-publish", "cli"];
 
 async function runApiExtractor(pkgName) {
 	const { Extractor, ExtractorConfig } = require("@microsoft/api-extractor");
