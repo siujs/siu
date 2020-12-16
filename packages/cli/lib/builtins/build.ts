6c4aa65f4b1d8d3b1d7324c515c73ad55c4234b1
buns <kuafujs@126.com>
2020-12-16 16:44:14 +0800

    fix(cli): adjust build output filename of fallback-build

--- packages/cli/lib/builtins/build.ts
+++ packages/cli/lib/builtins/build.ts
@@ -1,26 +1,30 @@
+import path from "path";
+
 import { asRollupPlugin, Config, SiuRollupBuilder, stopService, TOutputFormatKey } from "@siujs/builtin-build";
 import { HookHandlerContext, HookHandlerNext, plugin } from "@siujs/core";
-
-function transformConfig(config: Config) {
-	config.plugin("esbuild").use(asRollupPlugin(), [
-		{
-			sourcemap: true,
-			loaders: {
-				".js": "js",
-				".mjs": "js",
-				".cjs": "js",
-				".ts": "ts"
-			}
-		}
-	]);
-}
 
 export function asBuildFallback() {
 	const plug = plugin();
 
 	plug.build.process(async (ctx: HookHandlerContext, next: HookHandlerNext) => {
-		const builder = new SiuRollupBuilder(ctx.pkg(), {
-			onConfigTransform: transformConfig
+		const pkgData = ctx.pkg();
+
+		const builder = new SiuRollupBuilder(pkgData, {
+			onConfigTransform: (config: Config, format: TOutputFormatKey) => {
+				config.plugin("esbuild").use(asRollupPlugin(), [
+					{
+						sourcemap: true,
+						loaders: {
+							".js": "js",
+							".mjs": "js",
+							".cjs": "js",
+							".ts": "ts"
+						}
+					}
+				]);
+
+				config.output(format).file(path.resolve(pkgData.path, `dist/index.${format === "es" ? "mjs" : "cjs"}`));
+			}
 		});
 
 		const format = ctx.opts<string>("format");
